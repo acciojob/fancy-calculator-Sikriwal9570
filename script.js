ca674dfe002307feadb3d917c6a2853aecdd596a
Sikriwal9570 <128585704+Sikriwal9570@users.noreply.github.com>
2023-11-22 09:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+// Take input for the first variable and multiply it by 2
+let firstInput = prompt("Enter the first positive integer:");
+firstInput = parseInt(firstInput);
+
+// Check if the input is a positive integer
+if (isNaN(firstInput) || firstInput <= 0) {
+  alert("Please enter a valid positive integer for the first input.");
+} else {
+  let multipliedResult = firstInput * 2;
+
+  // Take input for the second variable
+  let secondInput = prompt("Enter the second positive integer:");
+  secondInput = parseInt(secondInput);
+
+  // Check if the input is a positive integer
+  if (isNaN(secondInput) || secondInput <= 0) {
+    alert("Please enter a valid positive integer for the second input.");
+  } else {
+    // Add the second input to the result of the first input multiplied by 2
+    let finalResult = multipliedResult + secondInput;
+
+    // Display the final result using alert()
+    alert("The final result is: " + finalResult);
+  }
+}
+
